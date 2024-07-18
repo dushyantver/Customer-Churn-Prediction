62b7e6927104d51e518551ba762b28fd18a11a37
Dushyant kumar verma <91840332+dushyantver@users.noreply.github.com>
2024-07-18 17:29:11 +0530

    exception created

---
+++ src/constants/training_pipeline/__init__.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+from typing import List
+
+## Data Ingestion Constant
+
+TIMESTAMP: datetime=datetime.now().strftime('%m_%d_%Y_%H_%M_%S') 
---
+++ src/exception.py
@@ -0,0 +1,23 @@
+import os
+import sys
+
+def error_message_detail(error: Exception, error_detail: sys) -> str:
+    _, _, exc_tb = error_detail.exc_info()
+
+    file_name: str = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+
+    error_message: str = "Error occurred python script name [{0}] line number [{1}] error message [{2}]".format(
+        file_name, exc_tb.tb_lineno, str(error)
+    )
+
+    return error_message
+
+class Customer_Exception(Exception): ## Inheritance from Exception class
+    def __init__(self, error_message, error_detail): # Constructor
+        super().__init__(error_message) ## This line calls the initializer/Constructor of the base Exception class, passing error_message to it.
+        self.error_message: str = error_message_detail(
+            error_message, error_detail=error_detail
+        )
+    def __str__(self):
+        return self.error_message
+
